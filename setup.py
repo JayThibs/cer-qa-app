ba6a4cb12ce489cc879606ae08f92c6ffd9b602b
JayThibs <thibo.jacques@gmail.com>
2021-12-01 13:19:12 -0700

    Added cookiecutter template.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a streamlit app where users can ask natural language questions about the CER ESAs and get back an answer as well as the paragraph where the answer was found.',
+    author='Jacques Thibodeau',
+    license='MIT',
+)
