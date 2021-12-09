d35540156cf0b2c5ab675e8af1ffa89a30bf89eb
JayThibs <thibo.jacques@gmail.com>
2021-12-09 09:23:47 -0700

    Updated source code path.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='cer-qa',
     packages=find_packages(),
     version='0.1.0',
     description='This is a streamlit app where users can ask natural language questions about the CER ESAs and get back an answer as well as the paragraph where the answer was found.',
